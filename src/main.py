02033f51888f25d63c2bb1b527908183bf075464
claudioDema99 <5433737@studenti.unige.it>
2022-11-09 23:14:18 +0100

    abbellimenti

--- src/main.py
+++ src/main.py
@@ -20,13 +20,13 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 # %%
 
-msg = {'info generali' : "Lunedì 31 Ottobre\nOrario: dalle 10.30 alle 4.30\n\nPrezzo uomo: 20€\nPrezzo donna: 15€\n\nL'ingresso é consentito esclusivamente con iscrizione in lista, e include l'OPEN BAR dalle ore 10.30 alle ore 2.00.\n\nEcc Ecc.. porca merdaaaaaaa",
-             'location' : 'Nuova Location: NoSpace Club, Via Vergnano 65, Brescia\n\n https://maps.app.goo.gl/34daiXjMFQM5x8aB7 \n\nMolto figo e accogliente, adatto alle famiglie.',
-             'musica' : 'Programma della serata:\n\nDalle 10.30 alle 00.00: House/commerciale -> Dj Trilly\nDalle 00.00 alle 2.00: Salsa e bachata -> Dj Gonzales El Gonzo\nDalle 2.00 alle 4.30: Tecno e pasticche -> Dj SeNonTiDroghiFaiSchifo',
-             'drink open bar':"Drinks:\n\nAcqua\nAcqua Frizzante\nAqua\nAqua Con Gas",
-             'tavoli':"Tavoli disponibili:\n\nTavolo1..\n\nTavolo 2\n\nSi dai avete capito non ho sbatta ah no basta copiare e incollare\n\nBASE:\n150 (5 ingressi) —> include 1 bottiglia base + open bar per tutta la durata dell'evento\n300 (10 ingressi) —> include 2 bottiglie base + open bar per tutta la durata dell'evento\n\nPREMIUM:\n200 (5 ingressi) —> include 1 bottiglia premium (belvedere/hendrick's) + open bar per tutta la durata dell'evento\n400 (10 ingressi) —> include 2 bottiglie premium  (belvedere/hendrick's) + open bar per tutta la durata dell'evento\n\nVIP:\n250 (5 ingressi) —> include 1 bottiglia premium (belvedere/hendrick's) e 1 spumante  + open bar per tutta la durata dell'evento\n500 (10 ingressi) —> include 2 bottiglie premium  (belvedere/hendrick's) e 2 spumanti + open bar per tutta la durata dell'evento",
-             'iscrizione in lista':"Si va bene tutto, però quanto cazzo è bello Dema??!?",
-             'chi siamo':"Sisi ma io capisco tutto, però non ho piu sbatta\n\nComunque\n\nDema è proprio un toro ma minchia proprio un torotoro fessssss",}
+msg = {'INFO GENERALI' : "🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n         INFO GENERALI\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nLunedì 31 Ottobre\nOrario: dalle 10.30 alle 4.30\n\nPrezzo uomo: 20€\nPrezzo donna: 15€\n\nL'ingresso é consentito esclusivamente con iscrizione in lista, e include l'OPEN BAR dalle ore 10.30 alle ore 2.00.\n\nEcc Ecc.. porca merdaaaaaaa",
+             'LOCATION' : '🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n             LOCATION\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nNuova Location: NoSpace Club, Via Vergnano 65, Brescia\n\n https://maps.app.goo.gl/34daiXjMFQM5x8aB7 \n\nMolto figo e accogliente, adatto alle famiglie.',
+             'MUSICA' : '🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n                MUSICA\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nProgramma della serata:\n\nDalle 10.30 alle 00.00: House/commerciale -> Dj Trilly\nDalle 00.00 alle 2.00: Salsa e bachata -> Dj Gonzales El Gonzo\nDalle 2.00 alle 4.30: Tecno e pasticche -> Dj SeNonTiDroghiFaiSchifo',
+             'DRINK OPEN BAR':"🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n        DRINK OPEN BAR\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nDrinks:\n\nAcqua\nAcqua Frizzante\nAqua\nAqua Con Gas",
+             'TAVOLI':"🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n                TAVOLI\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nTavoli disponibili:\n\nTavolo1..\n\nTavolo 2\n\nSi dai avete capito non ho sbatta ah no basta copiare e incollare\n\nBASE:\n150 (5 ingressi) —> include 1 bottiglia base + open bar per tutta la durata dell'evento\n300 (10 ingressi) —> include 2 bottiglie base + open bar per tutta la durata dell'evento\n\nPREMIUM:\n200 (5 ingressi) —> include 1 bottiglia premium (belvedere/hendrick's) + open bar per tutta la durata dell'evento\n400 (10 ingressi) —> include 2 bottiglie premium  (belvedere/hendrick's) + open bar per tutta la durata dell'evento\n\nVIP:\n250 (5 ingressi) —> include 1 bottiglia premium (belvedere/hendrick's) e 1 spumante  + open bar per tutta la durata dell'evento\n500 (10 ingressi) —> include 2 bottiglie premium  (belvedere/hendrick's) e 2 spumanti + open bar per tutta la durata dell'evento",
+             'ISCRIZIONE IN LISTA':"🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n     ISCRIZIONE IN LISTA\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nSi va bene tutto, però quanto cazzo è bello Dema??!?",
+             'CHI SIAMO':"🔺🔺🔺🔺🔺🔺🔺🔺🔺🔺\n             CHI SIAMO\n🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻\n\nSisi ma io capisco tutto, però non ho piu sbatta\n\nComunque\n\nDema è proprio un toro ma minchia proprio un torotoro fessssss",}
 
 
 btns = [KeyboardButton(btn) for btn in msg.keys()]
@@ -49,7 +49,7 @@ async def echo(message: types.Message):
     try:
         await message.reply(msg[message.text])
     except:
-        await message.reply("non ho capito cosa vuoi dire")
+        await message.reply("Non ho capito cosa cazzo vuoi dire brutto minchioneoneone")
 
 
 
